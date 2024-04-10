28261ea07402cbb29c9a85f6321b6464cbbab987
21120538 <luuvinhquangqg@gmail.com>
2024-04-10 16:44:57 +0700

    Change backend url and add disable react devtolls on production

--- client-react/src/backendkey.ts
+++ client-react/src/backendkey.ts
@@ -1,3 +1,3 @@
-export const BACKEND_SERVER = "http://localhost:5000";
-export const CLIENT_SERVER = "http://localhost:5173";
-export const GOOGLE_API = "http://localhost:5000/auth/google"
+export const BACKEND_SERVER = "https://tictactoe-server.azurewebsites.net";
+export const CLIENT_SERVER = "https://tic-tac-toe-qaeq.onrender.com";
+export const GOOGLE_API = "https://tictactoe-server.azurewebsites.net/auth/google"
