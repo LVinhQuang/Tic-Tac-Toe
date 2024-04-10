76fd0b907adc9426400cb06b69998831e61733c9
21120538 <luuvinhquangqg@gmail.com>
2024-04-10 20:03:50 +0700

    update CORS server

--- Server/index.js
+++ Server/index.js
@@ -22,7 +22,7 @@ const dbUrl = process.env.DATABASE;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }));
 app.use(cors({
-    origin: "https://tic-tac-toe-qaeq.onrender.com",
+    origin: process.env.CLIENT_URL,
     credentials: true
 }));
 
